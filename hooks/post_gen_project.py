7f8582bb0603728b315412c7fdb6c47b76caa398
Daniel Merrison <daniel.merrison@edmentum.com>
2021-09-19 09:32:48 +0100

    Added files

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+#import subprocess
+
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+#subprocess.call(['git', 'init'])
+#
+#subprocess.call(['pip', 'install', '--user', 'pre-commit'])
+#subprocess.call(['pre-commit', 'install'])
+#
+#subprocess.call(['git', 'add', '*'])
+#subprocess.call(['git', 'commit', '-m', 'Initial commit'])
---
+++ {{cookiecutter.module_directory_name}}/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  type        = string
+  description = "Your name"
+  default     = "Aristotle"
+}
+
+variable "region" {
+  type        = string
+  description = "Default region used by the provider"
+  default     = {{ cookiecutter.aws_provider_region }}
+}
