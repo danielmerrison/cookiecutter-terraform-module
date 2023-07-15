c5808d52f59915b43dd0bc024d6e2ec05b0832a6
Daniel Merrison <90834472+danielmerrison@users.noreply.github.com>
2023-07-15 12:21:51 +0100

    Removed region, set terraform version dynamically, fix module path in example

--- {{cookiecutter.module_directory_name}}/examples/basic-usage/main.tf
+++ {{cookiecutter.module_directory_name}}/examples/basic-usage/main.tf
@@ -1,5 +1,18 @@
+terraform {
+  required_version = "{{ cookiecutter.terraform_version }}"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.59.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
 module "example" {
-  source = "../../../template-terraform-module"
+  source = "../../."
 
   name = "basic-usage"
 }
